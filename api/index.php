76faa6b6d62eb36d102d768653643971cfeb3f51
Joao Ventura <jcnventura-drupal@venturas.org>
2008-08-21 22:03:40 +0000

    Sync HEAD to 6.x

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Homepage of the API reference
+ */
+
+/** @mainpage Developer Documentation
+ * Welcome to the module developer's documentation. The interesting functions for
+ * themers are those that start with 'theme_'.
+ *
+ * - Components of Drupal
+ *   - @link print.module Printer-friendly version module @endlink
+ *   - @link print_mail.module Send by e-mail module @endlink
+ *   - @link print_pdf.module PDF version module @endlink
+ */
